69e84986daeedb3ddbf1de14b24d55b16967363d
Stanislaw <stan.dutkiewicz@hotmail.com>
2023-12-16 10:47:26 +0100

    implementacja Gaussa bez wyboru diagonalnego

--- lmp9/src/gauss.c
+++ lmp9/src/gauss.c
@@ -5,10 +5,27 @@
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
 
-		return 0;
+
+    int n = mat->r; // Pobranie liczbę wierszy macierzy
+
+    for (int i = 0; i < n; i++) {
+        
+        if (mat->data[i][i] == 0) {
+            return 1; // macierz osobliwa
+        }
+		// bez wybory elementu diagonalnego
+		
+        for (int j = i + 1; j < n; j++) {
+            //współczynnik do wyeliminowania elementów poniżej diagonalnego
+            double factor = mat->data[j][i] / mat->data[i][i];
+
+            
+            for (int k = i; k < n; k++) {
+                mat->data[j][k] -= factor * mat->data[i][k]; // wyeliminowanie elementów poniżej diagonalnego
+            }
+            b->data[j][0] -= factor * b->data[i][0];
+        }
+    }
+    return 0; // Sukces
 }
-
